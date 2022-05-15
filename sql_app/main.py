ee74cec7d343b6aa16900550d837da71328bfea1
hotohara <hot87hara@gmail.com>
2022-05-15 16:06:20 +0900

    add first upload

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, File, Form, UploadFile
 from sqlalchemy.orm import Session
 
 from . import crud, models, schemas
@@ -41,9 +41,7 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(
-    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
+def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
@@ -51,3 +49,9 @@ def create_item_for_user(
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+
+@app.post("/upload/")
+async def create_file(uploadfile: UploadFile):
+
+    return {"upload_filename": uploadfile.filename, "uploadfile_content_type": uploadfile.content_type}
